887c17b0eb8b7544876701b6d5ee9ca1f4cd0aec
nankaiwolf <45894355+nankaiwolf@users.noreply.github.com>
2021-03-12 22:41:55 +0800

    Update searchCoV.R

--- searchCoV.R
+++ searchCoV.R
@@ -1,20 +1,20 @@
-# 该患者RNA比对中发现有SARS-CoV种的注释，需要进行逆向工程提取原始序列进行分析。
+# 这些患者的RNA比对中发现有SARS-CoV的物种注释。
+# 为了确认这些序列的可靠性，就写了这段代码回溯找寻原始序列进行分析。
 # 读入mpa格式的物种表格
 tax.rna.count <- read.csv("tax.rna.mpa",
                           sep = "\t", header = F)
-# 提取其中Viruses的序列
+# 提取其中Viruses的序列数量
 virus.rna.count <- tax.rna.count[grep("d__Viruses", tax.rna.count[ , 1], invert = F), ]
-
-# 读取序列匹配表格
+# 读取序列注释表格
 reads.rna.table <- read.csv("tax.rna.kra",
                             sep = "\t", header = F)
-# 保留已经匹配的序列
+# 保留已经匹配（Classified）的序列
 classified.reads.rna <- reads.rna.table[grep("C", reads.rna.table[ , 1], invert = F), ]
 # 在kraken2注释里SARS-CoV2的物种编号是2697049
 # 这里大家先整体看看各个物种的数量情况
 summary(as.factor(classified.reads.rna[ , 3]))
 
-# 读入含有疑似序列的fastq文件，记得千万要去除读取注释，因为有的质控行可能以字符“#”开头。
+# 读入含有疑似序列的fastq文件，记得这一步千万要去除读取注释，因为有的质控行可能以字符“#”开头。
 reads.out <- read.table("seq.rna.fastq", sep = "\n", comment.char = "")
 # 把数据的四列分离出来。
 reads.data <- reads.out[as.numeric(rownames(reads.out))%%4 == 1, ]
